9a9ae4d8dc7c0c20f43345e2d9e6ed4ad5e8f198
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-08-02 20:12:20 +0530

    Logical_Operator Code Added

--- Logical_Operator.cpp
+++ Logical_Operator.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int a = 5, b = 8;
-    cout << (a <= b) && (5 != 8); // And Operator
+    cout << (a <= b) && (5 != 8); // AND Operator
     cout << endl;
     cout << (a < b) || (a == b); // OR Operator
     cout << endl;
