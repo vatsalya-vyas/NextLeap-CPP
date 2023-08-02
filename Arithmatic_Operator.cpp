b7e0b9b78ad63af934d159ee41cccbe79665e3f2
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-08-02 20:12:20 +0530

    Arithmatic Operator Code Added

--- Arithmatic_Operator.cpp
+++ Arithmatic_Operator.cpp
@@ -8,7 +8,7 @@ int main()
     // float c = a * b;
     // cout << "The sum is : " << c << endl;
 
-    // int ans = 5 * 2 / 4; // Associativity : Left => Right
+    // int ans = 5 * 2 / 4; // Associativity : Left ==> Right
     // int ans = 5 * (2 / 4); // Higest Precedence of the operator will execute first
 
     int a = 5, b = 3;
