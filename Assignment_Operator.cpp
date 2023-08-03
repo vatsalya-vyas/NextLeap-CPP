514719d257d68cc8b9aec96d8fb2ce673afda2db
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-08-03 20:12:20 +0530

    Assignment_Operator Code Added

--- Assignment_Operator.cpp
+++ Assignment_Operator.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    int a = 5; // 101
+    int a = 5; // Binary --> 101
 
     // a += 3;   // a=a+3
     // a %= 3;   // a=a%3
 
-    int b = a & 3; // 011
+    int b = a & 3; // Binary --> 011
 
     cout << b;
 
