8e715f7851fc74a80f11b07e94f8c7173831d331
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-08-11 20:12:20 +0530

    STL_Stack Code Added

--- STL_Stack.cpp
+++ STL_Stack.cpp
@@ -2,7 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{ // Start of the main function
+{
+    // Start of the main function
     stack<int> my_stack;
     my_stack.push(10);
     my_stack.push(15);
